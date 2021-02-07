d3b19d878bf5250ae9846a7d56b36f02102570b9
Hunterleeh <78357625+Hunterleeh@users.noreply.github.com>
2021-02-07 22:23:27 +0800

    Update example.py

--- example.py
+++ example.py
@@ -15,49 +15,37 @@ def f2c(f):
   return round(c, 2) # Keep 2 decimals
 
 def meter2foot(m):
-  if not isinstance(m, float):
-    return "ERROR -----> It is not a number"
-  if m < 0:
-    return "ERROR -----> It is a negative number"
+  if not isinstance(m, float) or m < 0:
+    return "ERROR -----> It is not a positive number"
   f = 0.3048 * m
   return round(f, 2) # Keep 2 decimals
 
 def foot2meter(f):
-  if not isinstance(f, float):
-    return "ERROR -----> It is not a number"
-  if f < 0:
-    return "ERROR -----> It is a negative number"
+  if not isinstance(f, float) or f < 0:
+    return "ERROR -----> It is not a positive number"
   m = 3.28 * f
   return round(m, 2) # Keep 2 decimals
 
 def inch2meter(i):
-  if not isinstance(i, float):
-    return "ERROR -----> It is not a number"
-  if i < 0:
-    return "ERROR -----> It is a negative number"
+  if not isinstance(i, float) or i < 0:
+    return "ERROR -----> It is not a positive number"
   m = 39.37 * i
   return round(m, 2) # Keep 2 decimals
 
 def meter2inch(m):
-  if not isinstance(m, float):
-    return "ERROR -----> It is not a number"
-  if m < 0:
-    return "ERROR -----> It is a negative number"
+  if not isinstance(m, float) or m < 0:
+    return "ERROR -----> It is not a positive number"
   i = 0.0254 * m
   return round(i, 4) # Keep 2 decimals
 
 def KgtoPound(kg):
-  if not isinstance(kg, float):
-    return "ERROR -----> It is not a number"
-  if kg < 0:
-    return "ERROR -----> It is a negative number"
+  if not isinstance(kg, float) or kg < 0:
+    return "ERROR -----> It is not a positive number"
   p = 0.454 * kg
   return round(p, 3) # Keep 2 decimals
 
 def PoundtoKg(p):
-  if not isinstance(p, float):
-    return "ERROR -----> It is not a number"
-  if p < 0:
-    return "ERROR -----> It is a negative number"
+  if not isinstance(p, float) or p < 0:
+    return "ERROR -----> It is not a positive number"
   kg = 2.2 * p
   return round(kg, 2) # Keep 2 decimals
