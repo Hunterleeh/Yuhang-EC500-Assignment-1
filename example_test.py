968c93df3d594589775550f2a89aa24bf0ce2789
Hunterleeh <78357625+Hunterleeh@users.noreply.github.com>
2021-02-07 21:21:23 +0800

    Update example_test.py

--- example_test.py
+++ example_test.py
@@ -6,7 +6,7 @@ def test_c2f():
   assert c2f(0) == 32.00
   assert c2f(100) == 212.00
   assert c2f(999) == 1830.20
-  assert some error here
+  #assert some error here
   assert c2f("String") == "ERROR -----> It is not a int"
   assert c2f("-123") == "ERROR -----> It is not a int"
   assert c2f("ec500") == "ERROR -----> It is not a int"
