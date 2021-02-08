68aa5fdd0898ab4703b01dc5153420199e9770bb
Hunterleeh <78357625+Hunterleeh@users.noreply.github.com>
2021-02-08 22:47:23 +0800

    Update example.py

--- example.py
+++ example.py
@@ -9,15 +9,15 @@ units=['meter', 'meters',
 
 # Fahrenheit = (Celsius * 9/5) + 32
 def c2f(c):
-  if not isinstance(c, int):
-    return "ERROR -----> It is not a int"  
+  if not isinstance(c, int) and not isinstance(c, float):
+    return "ERROR -----> It is not a number"  
   f = (c * 9 / 5) + 32
   return round(f, 2) # Keep 2 decimals
 
 # Celsius = (Fahrenheit – 32) * 5/9
 def f2c(f):
-  if not isinstance(f, int):
-    return "ERROR -----> It is not a int"
+  if not isinstance(c, int) and not isinstance(c, float):
+    return "ERROR -----> It is not a number"
   c = (f - 32) * 5 / 9
   return round(c, 2) # Keep 2 decimals
 
