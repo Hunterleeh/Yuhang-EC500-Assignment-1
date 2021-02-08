08bd030ff12052520a889d4547287351da41b288
Hunterleeh <78357625+Hunterleeh@users.noreply.github.com>
2021-02-08 22:04:09 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,11 @@
-import numpy as np
+import string
+
+units=['meter', 'meters',
+       'foot', 'feet',
+       'inch', 'inches',
+       'kilogram', 'kilograms',
+       'pound', 'pounds',
+       'Celsius', 'Fahrenheit']
 
 # Fahrenheit = (Celsius * 9/5) + 32
 def c2f(c):
@@ -30,7 +37,7 @@ def foot2meter(f):
   m = 3.28 * f
   return round(m, 2) # Keep 2 decimals
 
-def inch2meter(i):
+def meter2inch(i):
   if not isinstance(i, float) and not isinstance(i, int):
     return "ERROR -----> It is not a positive number"
   if i < 0:
@@ -38,7 +45,7 @@ def inch2meter(i):
   m = 39.37 * i
   return round(m, 2) # Keep 2 decimals
 
-def meter2inch(m):
+def inch2meter(m):
   if not isinstance(m, float) and not isinstance(m, int):
     return "ERROR -----> It is not a positive number"
   if m < 0:
@@ -61,3 +68,36 @@ def PoundtoKg(p):
     return "ERROR -----> It is not a positive number"
   kg = 2.2 * p
   return round(kg, 2) # Keep 2 decimals
+
+#The program has a fixed stucture: What is xxx xxx in xxx
+def breaker(texture):
+  information = [0, '', '']
+  information[0] = float(texture.split()[2])
+  information[1] = texture.split()[3]
+  information[2] = texture.split()[5]
+  return information
+
+def convertor(num, source_unit, goal_unit):
+  if (source_unit == units[0] or source_unit == units[1]) and (goal_unit == units[2] or goal_unit == units[3]):
+    return meter2foot(num)
+  elif (source_unit == units[2] or source_unit == units[3]) and (goal_unit == units[0] or goal_unit == units[1]):
+    return foot2meter(num)
+  elif (source_unit == units[0] or source_unit == units[1]) and (goal_unit == units[4] or goal_unit == units[5]):
+    return meter2inch(num) 
+  elif (source_unit == units[4] or source_unit == units[5]) and (goal_unit == units[0] or goal_unit == units[1]):
+    return inch2meter(num)
+  elif (source_unit == units[6] or source_unit == units[7]) and (goal_unit == units[8] or goal_unit == units[9]):
+    return KgtoPound(num)
+  elif (source_unit == units[8] or source_unit == units[9]) and (goal_unit == units[6] or goal_unit == units[7]):
+    return PoundtoKg(num)
+  elif source_unit == units[10] and goal_unit == units[11]:
+    return c2f(num)
+  elif source_unit == units[11] and goal_unit == units[10]:
+    return f2c(num)
+  else:
+    return "ERROR -----> You have inputed an invalid unit pair!"
+  
+#This is a demo!!!!!!!!
+#info = breaker("what is 100 inches in meters")
+#result = convertor(info[0], info[1], info[2])
+
